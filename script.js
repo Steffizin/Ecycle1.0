a5eea81409a0b68e340409c46bd9acbfd7246677
Steffizin <dudi.steff@gmail.com>
2025-04-15 15:46:04 -0300

    tela mapa

---
+++ script.js
@@ -0,0 +1,24 @@
+const map = L.map('map').setView([-23.620464473249893, -46.72967968674794], 16); // Coordenadas próximas da ETEC Abdias
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    }).addTo(map);
+
+    const iconeVerde = L.icon({
+      iconUrl: '../img/missoes/mis1.png',
+      iconSize: [32, 32],
+    });
+
+    const iconeLaranja = L.icon({
+      iconUrl: '../img/pontoLaranja.png',
+      iconSize: [32, 32],
+      className: 'orange-icon'
+    });
+
+    L.marker([-23.620014136933627, -46.731924025049615], { icon: iconeLaranja }).addTo(map)
+      .bindPopup('Você está aqui!');
+
+    L.marker([-23.62250601936956, -46.72886094244567], { icon: iconeVerde }).addTo(map)
+      .bindPopup('Ponto de coleta: Mercado e Bebidas Alves');
+
+    L.marker([-23.620464473249893, -46.72967968674794], { icon: iconeVerde }).addTo(map)
+      .bindPopup('Ponto de coleta: Escola Técnica Abdias');
